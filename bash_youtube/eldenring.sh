3532382f7ae800cc477c65d0de884ea9bf804ce1
Jordan Price <rj_price@hotmail.co.uk>
2023-10-31 17:43:16 +0000

    update ep4

--- bash_youtube/eldenring.sh
+++ bash_youtube/eldenring.sh
@@ -29,3 +29,25 @@ if [[ $beast == $tarnished ]]; then
 else
     echo "You Died"
 fi
+
+sleep 1
+
+echo "You walk further along the path towards the deserted village."
+sleep 2
+
+echo "You've reached the village square. It's eerily quiet..."
+sleep 1
+
+echo "An eldritch creature emerges, with enormous tattered wings and a demonic look."
+sleep 1
+
+echo "It's boss battle time. Get scared. Pick a number between 0-9."
+read tarnished
+
+beast=$(( $RANDOM $ 10 ))
+
+if [[ $beast == $tarnished ]]; then
+    echo "Beast VANQUISHED!! Congrats fellow tarnished."
+else
+    echo "You Died"
+fi
