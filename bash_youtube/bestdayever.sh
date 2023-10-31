c983ce89bee99552e85cfab81b81c11ffce43df6
Jordan Price <rj_price@hotmail.co.uk>
2023-10-31 08:38:44 +0000

    update ep2

--- bash_youtube/bestdayever.sh
+++ bash_youtube/bestdayever.sh
@@ -2,6 +2,10 @@
 
 compliment=$1
 
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
 echo "What is your name?"
 
 read name
@@ -15,3 +19,7 @@ echo "You're looking good today $name!!"
 sleep 1
 
 echo "You have the best $compliment I've ever seen $name!!"
+
+sleep 2
+
+echo "You are currently logged in as $user and are in the directory $whereami. Today is: $date."
