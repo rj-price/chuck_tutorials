f9f049d523339481c64afe0b5b4b19e062212c26
Jordan Price <rj_price@hotmail.co.uk>
2023-10-31 08:33:39 +0000

    update ep2

--- bash_youtube/bestdayever.sh
+++ bash_youtube/bestdayever.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Good Morning Jordan!!"
+compliment=$1
+
+echo "What is your name?"
+
+read name
+
+echo "Good Morning $name!!"
 
 sleep 1 
 
-echo "You're looking good today Jordan!"
+echo "You're looking good today $name!!"
 
 sleep 1
 
-echo "You have the best beard I've ever seen Jordan!!"
+echo "You have the best $compliment I've ever seen $name!!"
