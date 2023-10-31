c93204f92c2bc1162cc47d45b94b2427707ecff6
Jordan Price <rj_price@hotmail.co.uk>
2023-10-31 09:04:47 +0000

    update ep3

--- bash_youtube/getrichquick.sh
+++ bash_youtube/getrichquick.sh
@@ -7,3 +7,27 @@ echo "How old are you?"
 read age
 
 echo "Hello $name, you are $age years old."
+
+# Create own environmental variables
+: '
+To create your own environmental variables, add the following to .bashrc:
+
+export var="Some Text"
+
+'
+
+# Perform simple maths
+: '
+To perform simple maths in command line:
+
+echo $(( 3 + 4 ))
+echo $(( 3 * 4 ))
+echo $(( 3 / 4 ))
+echo $(( 3 % 4 ))
+
+'
+
+sleep 2
+
+getrich=$(( ($RANDOM % 15 ) + $age ))
+echo "You'll be a millionaire at $getrich."
