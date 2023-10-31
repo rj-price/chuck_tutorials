166794d4a1db814a3054392ee160a6d02b97a6d5
Jordan Price <rj_price@hotmail.co.uk>
2023-10-31 22:04:51 +0000

    update loreborne

--- bash_youtube/loreborne.sh
+++ bash_youtube/loreborne.sh
@@ -2,7 +2,9 @@
 
 # TO DO:
 #! Wrap whole game in while loop to allow player to continue
-#! Add in hp and attack attributes to battles
+#! Add random chance of retaliation after attack
+#! Add in damage multiplier if chose not to attack, but include option to heal
+#Add in hp and attack attributes to battles
 #! Develop story
 
 # Welcome
@@ -45,7 +47,7 @@ esac
 
 echo -e "Hello, $type. Your hp is $hp and your attack is $attack.\n"
 
-echo "Story type stuff goes here..."
+echo -e "Story type stuff goes here...\n"
 
 sleep 2
 
@@ -57,23 +59,48 @@ echo "Your first beast approaches."
 
 while [ $beast_hp -gt 0 ];
 do
-    echo "Will you attack? (y/n)"
+    echo -e "\nWill you attack? (y/n)"
     read action
     if [[ $action == "y" ]]; then
-        echo "You attack fiercely and injure the beast."
+        echo -e "\nYou attack fiercely and injure the beast."
         beast_hp=$(( $beast_hp - $attack ))
+        sleep 1
+        if [ $beast_hp -lt 0 ]; then
+            break
+        fi
+        echo "The beast retaliates. (hp -1)"
+        hp=$(( $hp - $beast_attack ))
+        sleep 1
+        echo -e "\nThe beast's hp is $beast_hp, your hp is $hp."
     else
         echo "The beast lashes out at you. (hp-1)"
         hp=$(( $hp - $beast_attack ))
-
-    echo "The beast's hp is $beast_hp, your hp is $hp."
-
+        echo "The beast's hp is $beast_hp, your hp is $hp."
+    fi
 done
 
-echo "Beast VANQUISHED!! Congratulations traveller."
+sleep 1
+echo -e "\n**** Beast VANQUISHED!! **** \nCongratulations traveller. Your hp is $hp.\n"
 
+sleep 2
 
-echo ""
+echo "In a nearby bush you find the remains of an unfortunate traveller. Lying next to the remains you find:
+1 - Cured meat (+2 hp)
+2 - Broadsword (+10 attack)
+"
+
+echo "Will you take anything? (1 or 2)"
+read pickup
+
+if [[ $pickup == "1" ]]; then
+    echo "You eat the cured meat. Your hp is now: $hp."
+elif [[ $pickup == "2" ]]; then
+    echo "You pickup the broadsword. Your attack is now: $attack."
+else
+    echo "You mutter your condolences and continue walking."
+fi
+
+echo -e "\n........."
 sleep 2
 
 echo "You walk further along the path towards the deserted village."
@@ -88,16 +115,30 @@ sleep 2
 echo "An eldritch creature emerges, with enormous tattered wings and a demonic look."
 sleep 2
 
-echo "It's boss battle time. Get scared. Pick an integer from 0 to 9. (0-9)"
-read tarnished
+# Boss battle 
+beast_hp=50
+beast_attack=3
 
-beast_hp=30
-beast_attack=1
+echo "It's boss battle time. Get scared."
 
-
-if [[ $beast == $tarnished ]]; then
-    echo "Beast VANQUISHED!! Congrats fellow tarnished."
-else
-    echo "You Died"
-    exit 1
-fi
+while [ $beast_hp -gt 0 ];
+do
+    echo -e "\nWill you attack? (y/n)"
+    read action
+    if [[ $action == "y" ]]; then
+        echo -e "\nYou attack fiercely and injure the beast."
+        beast_hp=$(( $beast_hp - $attack ))
+        sleep 1
+        if [ $beast_hp -lt 0 ]; then
+            break
+        fi
+        echo "The beast retaliates. (hp -3)"
+        hp=$(( $hp - $beast_attack ))
+        sleep 1
+        echo -e "\nThe beast's hp is $beast_hp, your hp is $hp."
+    else
+        echo "The beast lashes out at you. (hp-3)"
+        hp=$(( $hp - $beast_attack ))
+        echo "The beast's hp is $beast_hp, your hp is $hp."
+    fi
+done
