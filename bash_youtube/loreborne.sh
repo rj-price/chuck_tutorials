4e521751e7d47fc63e958d16cd4d5c9adf6feb2f
Jordan Price <rj_price@hotmail.co.uk>
2023-11-02 17:30:19 +0000

    lb update

--- bash_youtube/loreborne.sh
+++ bash_youtube/loreborne.sh
@@ -4,7 +4,7 @@
 # Intro to game ("Welcome to Loreborne, a game ...").
 # Add "Press ENTER to continue" sections.
 #! Wrap whole game in while loop to allow player to continue
-#! Add random chance of retaliation after attack
+# Add random chance of retaliation after attack
 #! Add in damage multiplier if chose not to attack, but include option to heal
 #Add in hp and attack attributes to battles
 #! Develop story
@@ -22,10 +22,10 @@ echo "██████████ ▀█████▀▄████▄    
 echo "                                                                                  "
 
 echo -e "In the world of Loreborne, an ancient evil plagues the land. Eldritch terrors roam
-the deserted villages, looking for their next victim. A lone traveller seeks revenge...\n"
+the deserted villages, looking for their next victim. A lone traveller seeks revenge..."
+echo ""
 
 read -p "Press ENTER to start..."
-
 echo ""
 
 echo -e "You enter a tavern in one of the few populated villages. The handful of patrons turn
@@ -39,7 +39,6 @@ echo "BAR KEEP: Welcome traveller. Please select your starting class:
 read class
 
 case $class in 
-
     1)
         type="Knight"
         hp=10
@@ -58,7 +57,6 @@ case $class in
 esac
 
 echo -e "Hello, $type. Your HP is $hp and your ATTACK is $attack.\n"
-
 sleep 2
 
 read -p "Press ENTER to leave the tavern and continue on your travels..."
@@ -68,6 +66,7 @@ sleep 2
 # First beast battle 
 beast_hp=30
 beast_attack=1
+beast_turn=$((RANDOM % 5))
 
 echo "As you walk from the village, an eerie feeling of foreboading settles upon you."
 sleep 2
@@ -84,26 +83,29 @@ do
         echo -e "\nYou attack fiercely and injure the beast."
         beast_hp=$(( $beast_hp - $attack ))
         sleep 1
-        if [ $beast_hp -lt 0 ]; then
+        # Check if the enemy is defeated
+        if [ $beast_hp -le 0 ]; then
+            echo -e "\n**** Beast VANQUISHED!! **** \nCongratulations traveller. Your hp is $hp.\n"
             break
         fi
-        echo "The beast retaliates. (hp -1)"
-        hp=$(( $hp - $beast_attack ))
+        # Randomly determine if the enemy attacks
+        if [[ $enemy_turn == 0 ]]; then
+            echo "The beast retaliates!"
+            hp=$(( $hp - ($beast_attack * 2) ))
+        else
+            echo "The beast hesitates..."
+        fi
         sleep 1
         echo -e "\nThe beast's hp is $beast_hp, your hp is $hp."
     else
-        echo "The beast lashes out at you. (hp-1)"
+        echo "The beast lashes out at you."
         hp=$(( $hp - $beast_attack ))
         echo "The beast's hp is $beast_hp, your hp is $hp."
     fi
 
 done
 
-sleep 1
-echo -e "\n**** Beast VANQUISHED!! **** \nCongratulations traveller. Your hp is $hp.\n"
-
 sleep 2
-
 echo "In a nearby bush you find the remains of an unfortunate traveller. Lying next to the remains you find:
 1 - Cured meat (+2 hp)
 2 - Broadsword (+10 attack)
@@ -113,8 +115,10 @@ echo "Will you take anything? (1 or 2)"
 read pickup
 
 if [[ $pickup == "1" ]]; then
+    hp=$(( $hp + 2 ))
     echo "You eat the cured meat. Your hp is now: $hp."
 elif [[ $pickup == "2" ]]; then
+    attack=$(( $attack + 10 ))
     echo "You pickup the broadsword. Your attack is now: $attack."
 else
     echo "You mutter your condolences and continue walking."
@@ -122,22 +126,19 @@ fi
 
 echo -e "\n........."
 sleep 2
-
 echo "You walk further along the path towards the deserted village."
 sleep 3
-
 echo "You've reached the village square. It's eerily quiet..."
 sleep 2
-
 echo "......"
 sleep 2
-
 echo "An eldritch creature emerges, with enormous tattered wings and a demonic look."
 sleep 2
 
 # Boss battle 
 beast_hp=50
 beast_attack=3
+beast_turn=$((RANDOM % 3))
 
 echo "It's boss battle time. Get scared."
 
@@ -149,16 +150,24 @@ do
         echo -e "\nYou attack fiercely and injure the beast."
         beast_hp=$(( $beast_hp - $attack ))
         sleep 1
-        if [ $beast_hp -lt 0 ]; then
+        # Check if the enemy is defeated
+        if [ $beast_hp -le 0 ]; then
+            echo -e "\n**** Beast VANQUISHED!! **** \nCongratulations traveller. Your hp is $hp.\n"
             break
         fi
-        echo "The beast retaliates. (hp -3)"
-        hp=$(( $hp - $beast_attack ))
+        # Randomly determine if the enemy attacks
+        if [[ $enemy_turn == 0 ]]; then
+            echo "The beast retaliates!"
+            hp=$(( $hp - ($beast_attack * 2) ))
+        else
+            echo "The beast hesitates..."
+        fi
         sleep 1
         echo -e "\nThe beast's hp is $beast_hp, your hp is $hp."
     else
-        echo "The beast lashes out at you. (hp-3)"
+        echo "The beast lashes out at you."
         hp=$(( $hp - $beast_attack ))
         echo "The beast's hp is $beast_hp, your hp is $hp."
     fi
+
 done
